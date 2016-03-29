9b20d0d17a5b4a11f596178c5c57f83ca294fd74
Lucas Ferreira <ljnferreira@gmail.com>
2016-03-29 15:50:29 -0300

    Updating the code with improvements in the code

--- cam_prototype/greenObjectDetector.py
+++ cam_prototype/greenObjectDetector.py
@@ -1,4 +1,3 @@
-#coding:utf-8
 from SimpleCV import *
 
 cam = Camera()
@@ -10,17 +9,28 @@ def detect_green_object():
         img = cam.getImage()
 
         dist = img.colorDistance(SimpleCV.Color.RED).dilate(2)
-        segmented = dist.stretch(200,255)
         blobs = dist.findBlobs(minsize=100)
         
         if blobs:
             blobs.draw()
+            
             x = 0
             y = 0
             for blob in blobs:
                 x = blob.coordinates()[0]
                 y = blob.coordinates()[1]
-            print '['+str(x)+','+str(y)+']'
-            img.drawRectangle(x-150,y-150,150,150,SimpleCV.Color.BLUE)
-            
-        img.show()    
+            if isGreen(img.crop(x,y,50,50)):
+            	print blob.coordinates()
+            	img.drawRectangle(x-150,y-150,150,150,SimpleCV.Color.BLUE)
+
+        img.show()
+
+def isGreen(crop):
+    (red, green, blue) = crop.splitChannels(False)
+    red_histogram = red.histogram()
+    green_histogram = green.histogram()
+    blue_histogram = blue.histogram()
+    if (green_histogram > blue_histogram) and (green_histogram > red_histogram):
+        return True
+    else:
+	    return False 
