218b8c1d67e0b98bdbbdf71f11c7832195c9892b
Dborah <deborahferreirafonseca@gmail.com>
2016-03-29 17:48:00 -0300

    findLines

---
+++ findLines/findLines.py
@@ -0,0 +1,34 @@
+ # coding=UTF-8
+
+from SimpleCV import *
+
+'''
+Identifica linhas retas em um streaming de vídeo
+'''
+def findLinesCam(): 
+	display = SimpleCV.Display()
+	cam = SimpleCV.Camera()
+	 
+	while display.isNotDone():
+	    img = cam.getImage().flipHorizontal()
+	    lines = img.findLines() 
+	    if (lines):
+		lines.daw((255,0,0))
+	    img.show()
+
+'''
+Identifica linhas retas em uma imagem
+'''
+def findLinesImage(img):
+	display = SimpleCV.Display()
+	
+	while display.isNotDone():
+	    img = Image(img).flipHorizontal()
+	    lines = img.findLines() 
+	    if (lines):
+		lines.draw((255,0,0))
+	    img.show()
+	
+
+# findLinesCam()
+findLinesImage('imagem.jpg')
