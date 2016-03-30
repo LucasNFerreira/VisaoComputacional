37a4fd0fb9b1e629ba48a23b15c373aa4bb3f88b
lucasnferreira <ljnferreira@gmail.com>
2016-03-30 17:13:57 -0300

    Update greenObjectDetector.py

--- cam_prototype/greenObjectDetector.py
+++ cam_prototype/greenObjectDetector.py
@@ -1,36 +1,28 @@
 from SimpleCV import *
 
 cam = Camera()
+x = 0
+y = 0
 
 def detect_green_object():
 
     while TRUE:
     
-        img = cam.getImage()
+        img = cam.getImage().flipHorizontal()
 
-        dist = img.colorDistance(SimpleCV.Color.RED).dilate(2)
-        blobs = dist.findBlobs(minsize=100)
+        dist = img.colorDistance(SimpleCV.Color.RED)
+        
+        segmented = dist.stretch(200,255)
+        blobs = segmented.findBlobs(minsize=50)
         
         if blobs:
             blobs.draw()
             
-            x = 0
-            y = 0
-            for blob in blobs:
-                x = blob.coordinates()[0]
-                y = blob.coordinates()[1]
-            if isGreen(img.crop(x,y,50,50)):
-            	print blob.coordinates()
-            	img.drawRectangle(x-150,y-150,150,150,SimpleCV.Color.BLUE)
-
+            blob = blobs[-1]
+            x = blob.coordinates()[0]
+            y = blob.coordinates()[1]
+            
+            print blob.coordinates()
+            img.drawRectangle(x-150,y-150,150,150,SimpleCV.Color.RED)
+            
         img.show()
-
-def isGreen(crop):
-    (red, green, blue) = crop.splitChannels(False)
-    red_histogram = red.histogram()
-    green_histogram = green.histogram()
-    blue_histogram = blue.histogram()
-    if (green_histogram > blue_histogram) and (green_histogram > red_histogram):
-        return True
-    else:
-	return False 
