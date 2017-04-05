4d465616e7eda3a61725f1edd9180c7c82abe2c1
LucasNFerreira <ljnferreira@gmail.com>
2017-04-04 21:38:08 -0300

    deleting some files

--- green_detector/teste.py
+++
@@ -1,8 +0,0 @@
-from twisted.web.server import Site
-from twisted.web.static import File
-from twisted.internet import reactor
-
-resource = File('mean/')
-factory = Site(resource)
-reactor.listenTCP(8080, factory)
-reactor.run()
